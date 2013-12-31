a52392f210c9030a0c958813260e9c888cecd426
Jeremy Shaw <jeremy@seereason.com>
2013-12-31 19:31:29 +0000

    clckwrks-plugin-bugs-0.6.5: update to work with theme changes in clckwrks 0.21

--- Clckwrks/Bugs/Page/Template.hs
+++ Clckwrks/Bugs/Page/Template.hs
@@ -23,24 +23,10 @@ template :: ( EmbedAsChild BugsM headers
          -> BugsM Response
 template ttl hdrs bdy =
     do p <- plugins <$> get
-       mTheme <- getTheme p
        (Just clckShowFn) <- getPluginRouteFn p (pluginName clckPlugin)
-       case mTheme of
-         Nothing      -> escape $ internalServerError $ toResponse $ ("No theme package is loaded." :: Text)
-         (Just theme) ->
-             do hdrXml <- fmap (map unClckChild) $ unXMLGenT $ asChild <%> <link rel="stylesheet" type="text/css" href=(BugsData "style.css") /> <% hdrs %></%>
-                bdyXml <- fmap (map unClckChild) $ unXMLGenT $ asChild bdy
-                fmap toResponse $ mapClckT f $ ClckT $ withRouteT (\f -> clckShowFn) $ unClckT $ unXMLGenT $ (_themeTemplate theme ttl hdrXml bdyXml)
+       hdrXml <- fmap (map unClckChild) $ unXMLGenT $ asChild <%> <link rel="stylesheet" type="text/css" href=(BugsData "style.css") /> <% hdrs %></%>
+       bdyXml <- fmap (map unClckChild) $ unXMLGenT $ asChild bdy
+       fmap toResponse $ mapClckT f $ ClckT $ withRouteT (\f -> clckShowFn) $ unClckT $ (themeTemplate p (ThemeStyleId 0) ttl hdrXml bdyXml)
     where
       f :: ServerPartT IO (a, ClckState) -> ReaderT BugsConfig (ServerPartT IO) (a, ClckState)
       f m = ReaderT $ \_ -> m
-
---       fmap toResponse $
---            themeTemplate p ttl <%> <link rel="stylesheet" type="text/css" href=(BugsData "style.css") /> <% hdrs %></%> bdy
-
---       let pageTemplate = undefined -- <- bugsPageTemplate <$> ask
---       undefined
-{-
-       fmap toResponse $ unXMLGenT $
-            pageTemplate ttl <%> <link rel="stylesheet" type="text/css" href=(BugsData "style.css") /> <% hdrs %></%> bdy
--}
